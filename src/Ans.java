7dc6bf76ec4d94425b7b65b818e95df7ed724b55
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-07-26 18:51:44 +0300

    Start/Master commit

--- src/Ans.java
+++ src/Ans.java
@@ -1,2 +1,9 @@
 public class Ans {
+    public static void main(String[] args) {
+        mostFrequendWeekdays(1997);
+    }
+    public static String[] mostFrequendWeekdays(int year){
+        String[] strings = new String[]{"Sunday"};
+        return strings;
+    }
 }
